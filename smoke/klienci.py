01628bee39a6b39494cc9f22ee1a3f1910db2d9b
Natalia <njablonska87@gmail.com>
2018-03-16 16:54:15 +0100

    Cała zakładka Klienci
    Signed-off-by: Natalia <njablonska87@gmail.com>

--- smoke/klienci.py
+++ smoke/klienci.py
@@ -5,28 +5,38 @@ from support_funcs import *
 
 # Xpaths
 
-# Kontrahenci logo
-__klienci_kontrahenci_logo = "//*[@id='content']/div/div/h1"
+# Logo
+__klienci_logo = "//*[@id='content']/div/div/h1"
 
-# Kontrahenci tabela
-__klienic_kontrahenci_tabela = "//*[@id='widget-grid']/div[2]/article/div[1]/table"
+# Tabela
+__klienic_tabela = "//*[@id='widget-grid']/div[2]/article/div[1]/table"
 
 # Oddzialy button
 __klienci_oddzialy_button = "//*[@id='left-panel']/nav/ul/li[2]/ul/li[2]/a"
 
-def check_klienci_kotrahenci_logo(driver_instance, text):
+# Osoby kontaktowe button
+__klienci_osoby_kontaktowe_button = "//*[@id='left-panel']/nav/ul/li[2]/ul/li[3]/a"
+
+# Ostrzeżenia button
+__klienci_ostrzezenia_button = "//*[@id='left-panel']/nav/ul/li[2]/ul/li[4]/a"
+
+# Sekretariat button in menu
+__pulpit_klienci_button_xpath = "//*[@id='left-panel']/nav/ul/li[2]/a/span"
+__pulpit_klienci_xpath = "//*[@id='left-panel']/nav/ul/li[2]/a"
+
+def check_klienci_logo(driver_instance, text):
     elem = wait_for_visibility_of_element(
         driver_instance=driver_instance,
-        xpath=__klienci_kontrahenci_logo
+        xpath=__klienci_logo
     )
     elem_text = elem.text
     print(elem_text)
     return elem_text == text
 
-def check_klienci_kontrahenci_tabela(driver_instance):
+def check_klienci_tabela(driver_instance):
     elem = wait_for_visibility_of_element(
         driver_instance=driver_instance,
-        xpath=__klienic_kontrahenci_tabela
+        xpath=__klienic_tabela
     )
     return elem
 
@@ -34,4 +44,16 @@ def hit_enter_on_oddzialy_button(driver_instance):
     click_enter_on_element(
         driver_instance=driver_instance,
         xpath=__klienci_oddzialy_button
+    )
+
+def hit_enter_on_osoby_kontaktowe_button(driver_instance):
+    click_enter_on_element(
+        driver_instance = driver_instance,
+        xpath = __klienci_osoby_kontaktowe_button
+    )
+
+def hit_enter_on_ostrzezenia_button(driver_instance):
+    click_enter_on_element(
+        driver_instance = driver_instance,
+        xpath = __klienci_ostrzezenia_button
     )
--- smoke/tests.py
+++ smoke/tests.py
@@ -41,17 +41,53 @@ class tests(unittest.TestCase):
             pulpit.check_hitting_results_are_correct_kontrahenci(self.driver, 'Kontrahenci')
         )
         pulpit.hit_enter_on_kontrahenci_button(self.driver)
-
+############# Kontrahenci #############
         self.assertTrue(
             klienci.check_url_contains_correct_name_page(self.driver, 'http://integracja.abraks.pl/customer/contractor')
         )
         self.assertTrue(
-            klienci.check_klienci_kotrahenci_logo(self.driver, 'Kontrahent > Indeks')
+            klienci.check_klienci_logo(self.driver, 'Kontrahent > Indeks')
         )
         self.assertTrue(
-            klienci.check_klienci_kontrahenci_tabela(self.driver)
+            klienci.check_klienci_tabela(self.driver)
         )
         klienci.hit_enter_on_oddzialy_button(self.driver)
+############## Oddzialy ###############
+        self.assertTrue(
+            klienci.check_url_contains_correct_name_page(self.driver, 'http://integracja.abraks.pl/customer/department')
+        )
+        self.assertTrue(
+            klienci.check_klienci_logo(self.driver, u'Oddział > Indeks')
+        )
+        self.assertTrue(
+            klienci.check_klienci_tabela(self.driver)
+        )
+        klienci.hit_enter_on_osoby_kontaktowe_button(self.driver)
+
+############## Osoby kontaktowe ###############
+
+        self.assertTrue(
+            klienci.check_url_contains_correct_name_page(self.driver, 'http://integracja.abraks.pl/customer/person')
+        )
+        self.assertTrue(
+            klienci.check_klienci_logo(self.driver, 'Osoba Kontaktowa > Indeks')
+        )
+        self.assertTrue(
+            klienci.check_klienci_tabela(self.driver)
+        )
+        klienci.hit_enter_on_ostrzezenia_button(self.driver)
+
+############## Ostrzezenia ###############
+        self.assertTrue(
+            klienci.check_url_contains_correct_name_page(self.driver, 'http://integracja.abraks.pl/customer/warning')
+        )
+        self.assertTrue(
+            klienci.check_klienci_logo(self.driver, u'Ostrzeżenie > Indeks')
+        )
+        self.assertTrue(
+            klienci.check_klienci_tabela(self.driver)
+        )
+     #   klienci.hit_enter_on_ostrzezenia_button(self.driver)
 
 
 if __name__ == "__main__":
